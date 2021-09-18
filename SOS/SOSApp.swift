706a76cdbcd77b8f7de3348a1ce87563d8769370
jotkafomat <k-j@xl.wp.pl>
2021-09-18 14:02:41 +0100

    Initial Commit

---
+++ SOS/SOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOSApp.swift
+//  SOS
+//
+//  Created by Krzysztof Jankowski on 18/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
